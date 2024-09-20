a777badbe0e748287737dc21a4dcfe021cd2930a
anovotex <asifhossain.khan19@gmail.com>
2024-09-20 09:10:34 +0600

    random dude is taken care of

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <Hero />
       <SectionProduct />
       <Book />
-      {/*<RandomDude />
-      <ShopCollection />
+      <RandomDude />
+      {/*<ShopCollection />
       <NewArrivals />
       <CapTshirt />
       <Belief />
